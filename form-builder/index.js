9fcb7d2972084b09d5b262a885aedc788eec0e7a
Md Afiq Iskandar <mdafiqiskandar@gmail.com>
2023-08-01 09:23:31 +0800

    Delete old formbuilder

--- form-builder/index.js
+++
@@ -1,7 +0,0 @@
-// Dynamic import from form-builder folder other than index.js
-
-import form1 from "./tMXqzWpcJ0H7p7L9V6OMKGBPNb81vr7X.js";
-
-export default {
-  form1,
-};
--- form-builder/tMXqzWpcJ0H7p7L9V6OMKGBPNb81vr7X.js
+++
@@ -1,4 +0,0 @@
-export default {
-  title: "Untitled Form",
-  description: "Form Description",
-};
